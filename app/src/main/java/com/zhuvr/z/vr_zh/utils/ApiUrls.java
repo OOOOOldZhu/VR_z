f3222a5127c62b8aabf219be76eb04273d40c4cd
z <1032671220@qq.com>
2017-10-08 23:45:00 +0800

    eo

--- app/src/main/java/com/zhuvr/z/vr_zh/utils/ApiUrls.java
+++ app/src/main/java/com/zhuvr/z/vr_zh/utils/ApiUrls.java
@@ -4,5 +4,7 @@ package com.zhuvr.z.vr_zh.utils;
 public class ApiUrls {
 
     public static final String IMAGE_LIST="http://louba365.com/server/index.php?g=Web&c=Mock&o=success&mockCode=rJGkqMXvfC1EuxrXBFrWzgG4CV63jlJf";
-    public static final String VIDEO_LIST="http://louba365.com/server/index.php?g=Web&c=Mock&o=success&mockCode=qcGQCMqEzghQqW61yeUWCCqR9AE9Nzql";
+
+    //eo账号1032。。@qq.com    密码  56.....
+    public static final String VIDEO_LIST="http://result.eolinker.com/d1vEEsX3607be1d3740433cef4f86005f49a56b5f4a8c54?uri=/v1.0";
 }
